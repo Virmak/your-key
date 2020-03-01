992e7eddb7f24dfeb953731284bf4bf1e43de8f6
Mohamed ben amar <medxbenamar@gmail.com>
2020-03-01 05:34:23 +0100

    added js collapse

---
+++ script.js
@@ -0,0 +1,19 @@
+let menuOpened = false;
+let menuCollapseElement;
+let menuElement;
+
+document.addEventListener('DOMContentLoaded', () => {
+    menuCollapseElement = document.querySelector('.menu-toggle');
+    menuElement = document.querySelector('.menu-collapse');
+    menuElement.style.display = 'none';
+    menuCollapseElement.addEventListener('click', toggleMenu);
+});
+
+const toggleMenu = () => {
+
+    menuElement.style.display = menuOpened ? 'none' : 'block';
+    menuOpened = !menuOpened;
+
+}
+
+document.addEventListener
